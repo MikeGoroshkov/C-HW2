e3033d316b2676529f4cbabc067c9794f4745cbc
Mikhail Goroshkov <gormike86@gmail.com>
2022-10-06 12:03:38 +0300

    finish

---
+++ 10/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+//показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+Console.Write("Введите трехзначное число: ");
+int n = int.Parse(Console.ReadLine());
+if (n >= 0 && n.ToString().Length != 3) { Console.WriteLine($"Число {n} не является трехзначным"); }
+else if (n < 0 && n.ToString().Length != 4) { Console.WriteLine($"Число {n} не является трехзначным"); }
+else
+{
+    int num2 = (n % 100) / 10;
+    if (num2 >= 0) { Console.WriteLine("Вторая цифра заданного числа: " + num2); }
+    else { Console.WriteLine("Вторая цифра заданного числа: " + -num2); }
+}
